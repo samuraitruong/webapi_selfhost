cba19d38c482dae4a48f8502caaca2fb3be8708f
Truong Nguyen <truong.nguyen@enett.com>
2016-03-31 16:47:38 +1100

    add unit test project

---
+++ SelfHost_Service.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SelfHost_Service.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Equals(1, 2);
+        }
+    }
+}
